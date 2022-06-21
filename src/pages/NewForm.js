236aa4e10e6731adf7ce0281afa0a8bab04e7a63
Jason <jasonwillhudson@gmail.com>
2022-06-20 23:40:45 -0700

    a component folder

--- src/pages/NewForm.js
+++ src/pages/NewForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import '../stylesheets/newform.css'
 
 const NewForm = () => {
     return (
-        <div>
-            <h1>New Form</h1>
+        <div className = "requestForm">
+            <form action="/addNewRequest" method="post">
+            <div>
+            <input type="text" name="title" value="" placeholder="Title of Event" />
+            <input type="number" name="amount" value="" placeholder="Bill Amount" />
+            <input type="date" name="title" placeholder="Date" />
+                <input type="submit" name="sub" value="Submit" />
+            </div>
+            </form>
         </div>
     );
 };
