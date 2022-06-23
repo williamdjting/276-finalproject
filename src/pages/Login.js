2d6bd890e3ce7db35b3e06ecac9db55c7a4d706d
Jason <jasonwillhudson@gmail.com>
2022-06-23 01:05:35 -0700

    register page done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../stylesheets/newform.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -10,17 +11,23 @@ const Login = () => {
           <input type="text" name="username" placeholder="Email or Username" />
           <input type="password" name="password" placeholder="Password" />
 
-          <input type="submit" name="login" value="Log In" />
+          <input
+            type="button"
+            name="login"
+            value="Log In"
+            onClick={() => (window.location.href = "/")}
+          />
 
           <input
-            type="submit"
+            type="button"
             name="signup"
             value="Sign Up"
             className="signUp"
+            onClick={() => (window.location.href = "/signup")}
           />
 
           <a
-            href="/"
+            href="/login"
             style={{
               fontSize: "14px",
               alignSelf: "flex-end",
