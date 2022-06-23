9c7247bcca506eb22f97d31eee1a32eee50aa71e
Jason <jasonwillhudson@gmail.com>
2022-06-22 21:00:45 -0700

    new form page done

---
+++ src/components/newForm/PeopleForm.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "../../stylesheets/newform.css";
+import deleteIcon from "../../icons/close.svg";
+
+const PeopleForm = ({ passData }) => {
+  const [input, setInput] = React.useState("");
+  const [userList, setUserList] = React.useState([]);
+
+  const saveInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const addNewUser = () => {
+    if (userList.indexOf(input) == -1) {
+      setUserList((prevState) => [...prevState, input]);
+    }
+  };
+
+  useEffect(() => {
+    passData(userList);
+  }, [userList]);
+  /*
+  const removeUser = (index) => {
+    setTimeout(() => {
+      setUserList([
+        ...userList.slice(0, index),
+        ...userList.slice(index + 1, userList.length),
+      ]);
+    }, 1);
+  };
+*/
+  return (
+    <div className="formContent padding border">
+      <h3>Add People</h3>
+      <div style={{ width: "350px" }}>
+        <input
+          type="text"
+          name="addUserTo"
+          value={input}
+          onChange={saveInput}
+          placeholder="Email Address"
+        />
+        <button type="button" onClick={addNewUser}>
+          Add
+        </button>
+      </div>
+
+      {/* Interactive User List */}
+      <TransitionGroup component="ul">
+        {userList.reverse().map((item, index) => (
+          <CSSTransition timeout={500} key={item} classNames="userInFormList">
+            <li key={item}>
+              <p>{item}</p>
+
+              <img
+                src={deleteIcon}
+                alt="delete"
+                className="deleteIcon"
+                onClick={() => {
+                  setUserList(userList.filter((t) => t !== item));
+                  passData(userList);
+                }}
+              />
+            </li>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </div>
+  );
+};
+export default PeopleForm;
--- src/pages/NewForm.js
+++ src/pages/NewForm.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import '../stylesheets/newform.css'
+import React, { useState } from "react";
+import "../stylesheets/newform.css";
+import deleteIcon from "../icons/close.svg";
+import PeopleForm from "../components/newForm/PeopleForm";
 
 const NewForm = () => {
-    return (
-        <div className = "requestForm">
-            <form action="/addNewRequest" method="post">
-            <div>
-            <input type="text" name="title" value="" placeholder="Title of Event" />
-            <input type="number" name="amount" value="" placeholder="Bill Amount" />
-            <input type="date" name="title" placeholder="Date" />
-                <input type="submit" name="sub" value="Submit" />
-            </div>
-            </form>
+  const [type, setType] = useState("text");
+  const [userListData, setUserListData] = useState([]);
+
+  const passData = (data) => {
+    setUserListData(data);
+    //console.log("parent: " + data);
+  };
+
+  return (
+    <div className="requestForm">
+      <form>
+        <div className="formContent padding border">
+          <h2>Form</h2>
+          <input type="text" name="title" placeholder="Title of Event" />
+          <input type="number" name="amount" placeholder="Bill Amount" />
+          <input
+            type={type}
+            name="title"
+            placeholder="YYYYMM/DD"
+            onFocus={() => setType("date")}
+            onBlur={() => setType("text")}
+          />
+          <input type="submit" name="submit" value="Send" />
         </div>
-    );
+        <input type="hidden" name="userList" value={userListData} />
+        <PeopleForm passData={passData} />
+      </form>
+    </div>
+  );
 };
 export default NewForm;
