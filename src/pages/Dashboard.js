09d02461fdd79a0f04ca49d8d31a3e8b6423d671
William Ting <williamdjting@gmail.com>
2022-06-28 18:21:39 -0700

    added react skeleton

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,48 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+            <table>
+                <tr>
+                    <td id="placeholder11">
+                        <div>
+                        [Component Placeholder: Row 1, Col 1]
+                        </div>
+                    </td>
+                    <td id="placeholder12">
+                        <div>
+                        [Component Placeholder: Row 1, Col 2]
+                        </div>
+                    </td>
+                    <td id="placeholder13">
+                        <div>
+                        [Component Placeholder: Row 1, Col 3]
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td id="placeholder21">
+                        <div>
+                        [Component Placeholder: Row 2, Col 1]
+                        </div>
+                    </td>
+                    <td id="placeholder22">
+                        <div>
+                        [Component Placeholder: Row 2, Col 2]
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td id="placeholder31">
+                        <div>
+                        [Component Placdeholder: Row 3, Col 1, for dummy submit form]
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        
         </div>
     );
 };
