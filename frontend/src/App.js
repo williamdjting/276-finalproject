a8a95705dca538eecaf1df2c487aa50a96d9bf32
William Ting <williamdjting@gmail.com>
2022-07-06 13:56:05 -0700

    added /dashboard route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,6 +37,7 @@ function App() {
             }
           >
             <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/newForm" element={<NewForm />} />
           </Route>
         </Routes>
