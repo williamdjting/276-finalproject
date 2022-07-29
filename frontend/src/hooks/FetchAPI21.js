dbbeda10151e509612bc911a42253d3d84bb5502
jasonwillhudson <38706702+jasonwillhudson@users.noreply.github.com>
2022-07-29 01:05:21 -0700

    history user undefined bug fixed

--- frontend/src/hooks/FetchAPI21.js
+++ frontend/src/hooks/FetchAPI21.js
@@ -42,7 +42,7 @@ function FetchAPI21() {
           <tr key={item.reqid}>
             <td>{item.date.substring(0, 10)} </td>
             <td>{item.title} </td>
-            <td>{item.req_sent ? ("to " + item.receiverid.split(", ").length + users):("from " + item.receiverid)}</td>
+            <td>{item.req_sent ? ("to " + item.receiverid.split(", ").length):("from " + item.receiverid)}</td>
             <td >{"$"+item.amount}</td>
             <a href=""> view more </a>
           </tr>
